4c053d9b8f87fdddab58f100c29a629cf44aa3da
Rob Richards <goflyakite@gmail.com>
2021-02-03 19:07:40 -0800

    Working eosio table.

--- READMD.md
+++ READMD.md
@@ -1,3 +1,17 @@
-# survey
+# Survey, instead of Surveillance
 
 My first eosio coding, basically like a hello world program.
+
+This is from the eosio course at: https://training.eos.io/courses/take/smart-contracts-101 I have opted to place it here because I had troubles getting it to work at first and found no one who had it available for me to reference. I did email  "support" but got a resounding emptiness.
+
+In my code I don't like the word surveillance, it feels creepy, quite long, and I do not like how it's spelled.
+
+I have opted to shorten "surveillance" to "survey".
+
+
+## History
+
+Good milestones.
+
+cb40468 - Initial commit, working hello.
+
--- src/survey.cpp
+++ src/survey.cpp
@@ -13,10 +13,32 @@ CONTRACT survey : public contract {
         if ( user.value == 3773036822876127232) {
           print ("\nHello Alice!");
         } else {
-          print ("\You are not Alice!");
+          print ("\nYou are not Alice!");
         }
+
+        survey_index survey(get_self(), get_self().value );
+        //                  ^Code       ^Scope
+        //              vPayer       v Lamba fn
+        survey.emplace( get_self(), [&]( auto& new_row) {
+          new_row.subject = user;
+          new_row.timestamp = current_time_point();
+        });
     };
 
   private:
-  
+    //struct [[eosio::table]] survey_record {
+    // FYI: the above line is the same as the below line. Pick one.
+    TABLE survey_record { 
+        name subject;
+        eosio::time_point timestamp;
+
+        uint64_t primary_key() const { return subject.value; }
+        EOSLIB_SERIALIZE(survey_record, (subject)(timestamp))
+    };
+
+    typedef eosio::multi_index<name("survey"), survey_record> survey_index;
+    //FYI: This following statement does exactly the same as the one above.
+    //using survey_index=eosio::multi_index<name("survey"), survey_record>;
+
+    //something_index _something; 
 };
